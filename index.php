a81eeea6a871194e1a9da5ea094bd50f257f2e6d
LouisHyt <louishayotte@gmail.com>
2023-06-29 10:41:16 +0200

    small update

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ajout produit</title>
+    <title>Ajouts produit</title>
 </head>
 <body>
     <h1>Ajouter un produit</h1>
